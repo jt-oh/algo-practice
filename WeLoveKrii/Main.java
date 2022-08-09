116f4a0b3e59ecd82c625f3c2d0b9870fd14ee2a
JeongTaek Oh <tei1234tei@gmail.com>
2022-08-10 00:25:40 +0900

    WeLoveKrii

---
+++ WeLoveKrii/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String... args) {
+        int[] range = {0, 1};
+
+        for (int i : range) {
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
