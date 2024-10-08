bfb2898cdb41757223602dbfa205251996898e4c
JeongTaek Oh <tei1234tei@gmail.com>
2024-10-08 16:46:20 +0900

    remove unused imported class

--- AirPurifier/Main.java
+++ AirPurifier/Main.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 
 public class Main {
     private class RoomPosition {
